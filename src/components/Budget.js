054ab4b5df73b0300090d5f79c521b72d42dbeb8
Napoleon Posada <napoleonposada@gmail.com>
2024-06-29 00:50:47 -0400

    Examen final de React

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,1 +1,30 @@
+import React, { useContext, useState } from 'react';
+import { AppContext } from '../context/AppContext';
+
+const Budget = () => {
+    const { budget,remaining, currency } = useContext(AppContext);
+    const [newBudget, setNewBudget] = useState(budget);
+    const handleBudgetValidation = (event) => {
+        if(event.target.value < remaining){
+            alert("The budget value cannot set under " + remaining);
+            event.target.value = budget;
+        }
+        else if (event.target.value > 20000){
+            alert("The budget value cannot exceed el valor de " + currency + " 20,000");
+            event.target.value = budget;
+        }
+        else
+            setNewBudget(event.target.value);
+    }
+    const handleBudgetChange = (event) => {
+        setNewBudget(event.target.value);
+    }
+    return (
+        <div className='alert alert-secondary'>
+        <span>Budget: {currency}</span>
+        <input type="number" step="10" value={newBudget} onChange={handleBudgetChange} onBlur={handleBudgetValidation}></input>
+        </div>
+    );
+};
+export default Budget;
 
